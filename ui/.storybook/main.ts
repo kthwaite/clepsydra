9b0aada9954a63829c7108063b9dcbe500840692
Kit Thwaite <28732700+kthwaite@users.noreply.github.com>
2026-02-08 22:53:17 +0000

    chore(ui): add storybook config and ignore build artifacts

---
+++ ui/.storybook/main.ts
@@ -0,0 +1,13 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [],
+  framework: "@storybook/react-vite",
+  core: {
+    disableTelemetry: true,
+  }
+};
+export default config;
